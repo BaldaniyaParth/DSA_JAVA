5e8d9149f2d1de3f66333b58082020d2c594c44e
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-01-07 20:55:34 +0530

    DSA Question Solution

---
+++ TrappingRainWater.java
@@ -0,0 +1,16 @@
+public class TrappingRainWater {
+
+    public static void main(String[] args) {
+        int [] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(height));
+    }
+    public static int trap(int[] height) {
+        int l = 0, r = height.length - 1, lmax = Integer.MIN_VALUE, rmax = Integer.MIN_VALUE, ans = 0;
+        while (l < r) {
+            lmax = Math.max(lmax, height[l]);
+            rmax = Math.max(rmax, height[r]);
+            ans += (lmax < rmax) ? lmax - height[l++] : rmax - height[r--];
+        }
+        return ans;
+    }
+}
