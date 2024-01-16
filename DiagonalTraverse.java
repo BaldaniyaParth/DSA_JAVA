780d3cf9d1de1be9b67408d83271df689ddb394c
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-01-16 22:05:41 +0530

    Week 1 Day 2 Leetcode DSA Question Solution

---
+++ DiagonalTraverse.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class DiagonalTraverse {
+
+    public static void main(String[] args) {
+        int [][] mat = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(Arrays.toString(findDiagonalOrder(mat)));
+    }
+    public static int[] findDiagonalOrder(int[][] mat) {
+        if (mat.length == 0 || mat[0].length == 0) {
+            return new int[0];
+        }
+
+        int rows = mat.length;
+        int cols = mat[0].length;
+        int[] result = new int[rows * cols];
+        int r = 0;
+        int c = 0;
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = mat[r][c];
+            if ((r + c) % 2 == 0) {
+                if (c == cols - 1) {
+                    r++;
+                } else if (r == 0) {
+                    c++;
+                } else {
+                    r--;
+                    c++;
+                } } else {
+                if (r == rows - 1) {
+                    c++;
+                } else if (c == 0) {
+                    r++;
+                } else {
+                    r++;
+                    c--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ SqrtX.java
@@ -0,0 +1,13 @@
+public class SqrtX {
+
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println(mySqrt(x));
+    }
+    public static int mySqrt(int x) {
+        long r = x;
+        while (r*r > x)
+            r = (r + x/r) / 2;
+        return (int) r;
+    }
+}
