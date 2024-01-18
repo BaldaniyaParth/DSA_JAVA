2a3de18e2d3df7655a92f7c773ae7d8d778cbaf5
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-01-18 18:19:53 +0530

    Week 1 Day 4 Leetcode DSA Question Solution

---
+++ BitwiseANDNumbersRange.java
@@ -0,0 +1,17 @@
+public class BitwiseANDNumbersRange {
+
+    public static void main(String[] args) {
+        int left = 5;
+        int right = 7;
+        System.out.println(rangeBitwiseAnd(left,right));
+    }
+
+    public static int rangeBitwiseAnd(int left, int right) {
+        int res = right;
+        while(right > left) {
+            res = right & (right-1);
+            right = res;
+        }
+        return res;
+    }
+}
