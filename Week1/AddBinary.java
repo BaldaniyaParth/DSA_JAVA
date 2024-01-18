a8afb1faea05445973518bc1289a600751bcef18
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-01-18 18:23:52 +0530

    Week 1

--- Week1/AddBinary.java
+++ Week1/AddBinary.java
@@ -1,3 +1,5 @@
+package Week1;
+
 public class AddBinary {
 
     public static void main(String[] args) {
--- Week1/IndexOfFirstString.java
+++ Week1/IndexOfFirstString.java
@@ -1,3 +1,5 @@
+package Week1;
+
 public class IndexOfFirstString {
 
     public static void main(String[] args) {
