45b6c1a713027f9fa9bb88a3a26e3a673c68718b
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-02-02 18:43:10 +0530

    Day5 Striver DSA Sheet

---
+++ Day5/MaxConsecutiveOnes.java
@@ -0,0 +1,25 @@
+package Day5;
+
+public class MaxConsecutiveOnes {
+
+    public static void main(String[] args) {
+        int [] nums = {1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+            }else{
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+        max = Math.max(max, count);
+        return max;
+    }
+}
