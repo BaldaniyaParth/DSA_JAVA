42e69d442c7524d2733fe9621e22978762bb9ff7
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-02-06 22:04:34 +0530

    Day9 Striver DSA Sheet

---
+++ Day9/FirstLastPositionElementSortedArray.java
@@ -0,0 +1,61 @@
+package Day9;
+
+import java.util.Arrays;
+
+public class FirstLastPositionElementSortedArray {
+
+    public static void main(String[] args) {
+        int [] nums = {5,7,7,8,8,10};
+        int target = 8;
+        System.out.println(Arrays.toString(searchRange(nums, target)));
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        if (first == -1) return new int[] { -1, -1};
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    public static int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    first = mid;
+                    break;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return first;
+    }
+
+    public static int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                    last = mid;
+                    break;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return last;
+    }
+}
