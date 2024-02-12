0193b95b188586d0b67c48d43b6637d2bdc38543
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-02-12 17:24:35 +0530

    Day15 Striver DSA Sheet

---
+++ Day15/CountPrimes.java
@@ -0,0 +1,32 @@
+package Day15;
+
+import java.util.Arrays;
+
+public class CountPrimes {
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(countPrimes(n));
+    }
+
+    public static int countPrimes(int n) {
+        if(n<=1) return 0;
+
+        boolean[] a = new boolean[n];
+        Arrays.fill(a, true);
+        a[0] = a[1] = false;
+        int cnt = 1;
+
+        for(int i=2; i<Math.sqrt(n); i++){
+            if(a[i]){
+                for(int j=i*i; j<n; j+=i){
+                    if(a[j]) cnt++;
+                    a[j] = false;
+                }
+            }
+        }
+
+        return n-cnt-1;
+
+    }
+}
