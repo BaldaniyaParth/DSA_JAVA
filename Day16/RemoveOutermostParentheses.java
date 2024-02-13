2828b2e0e985473d307f35454b87b2b849a46fa7
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-02-13 19:06:09 +0530

    Day16 Striver DSA Sheet

---
+++ Day16/RemoveOutermostParentheses.java
@@ -0,0 +1,30 @@
+package Day16;
+
+public class RemoveOutermostParentheses {
+
+    public static void main(String[] args) {
+        String s = "(()())(())";
+        System.out.println(removeOuterParentheses(s));
+    }
+
+    public static String removeOuterParentheses(String s) {
+        int count = 0;
+        StringBuilder result = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (count != 0) {
+                    result.append(c);
+                }
+                count++;
+            } else {
+                if (count != 1) {
+                    result.append(c);
+                }
+                count--;
+            }
+        }
+
+        return result.toString();
+    }
+}
