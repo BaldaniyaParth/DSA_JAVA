5443cbe6b1994da0c34dcce5ff752959e2392d00
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-02-26 15:33:26 +0530

    Day29 Striver DSA Sheet

---
+++ Day29/WordBreak.java
@@ -0,0 +1,34 @@
+package Day29;
+
+import java.util.List;
+
+public class WordBreak {
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        String [] wordDict = {"leet","code"};
+        System.out.println(wordBreak(s, List.of(wordDict)));
+    }
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        int max_len = 0;
+        for (String word : wordDict) {
+            max_len = Math.max(max_len, word.length());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i - 1; j >= Math.max(i - max_len - 1, 0); j--) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+
+    }
+}
