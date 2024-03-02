064d3c8438e7bccc57c0e1fba39e03bd8c6edbdc
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-02 17:08:37 +0530

    Day34 Striver DSA Sheet

---
+++ Day34/AsteroidCollision.java
@@ -0,0 +1,32 @@
+package Day34;
+
+import java.util.Arrays;
+
+public class AsteroidCollision {
+
+    public static void main(String[] args) {
+        int [] asteroids = {5,10,-5};
+        System.out.println(Arrays.toString(asteroidCollision(asteroids)));
+    }
+
+    public static int[] asteroidCollision(int[] asteroids) {
+        int n = asteroids.length;
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            int asteroid = asteroids[i];
+            while (j>0 && asteroids[j-1]>0 && asteroid<0 && asteroids[j-1] < Math.abs(asteroid))
+            {j--;}
+
+            if (j==0 || asteroid>0 || asteroids[j-1]<0)
+                asteroids[j++] = asteroid;
+            else if(asteroids[j-1] == Math.abs(asteroid))
+                j--;
+        }
+
+        int[] result = new int[j];
+        System.arraycopy(asteroids, 0, result, 0, j);
+
+        return result;
+    }
+}
