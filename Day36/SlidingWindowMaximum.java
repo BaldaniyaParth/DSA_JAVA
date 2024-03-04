91a9f5e72be02ab8e2a1cd445e042fcf2df3bf0d
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-04 14:29:26 +0530

    Day36 Striver DSA Sheet

---
+++ Day36/SlidingWindowMaximum.java
@@ -0,0 +1,38 @@
+package Day36;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class SlidingWindowMaximum {
+
+    public static void main(String[] args) {
+        int [] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        System.out.println(Arrays.toString(maxSlidingWindow(nums, k)));
+    }
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] r = new int[n - k + 1];
+        int ri = 0;
+        // store index
+        Deque< Integer > q = new ArrayDeque< >();
+        for (int i = 0; i < n; i++) {
+            // remove numbers out of range k
+            if (!q.isEmpty() && q.peek() == i - k) {
+                q.poll();
+            }
+            // remove smaller numbers in k range as they are useless
+            while (!q.isEmpty() && nums[q.peekLast()] < nums[i]) {
+                q.pollLast();
+            }
+
+            q.offer(i);
+            if (i >= k - 1) {
+                r[ri++] = nums[q.peek()];
+            }
+        }
+        return r;
+    }
+}
