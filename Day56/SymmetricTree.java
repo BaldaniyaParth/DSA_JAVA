5e5aa0fda65a02b04239f6aa8d305aac5cfc9ecd
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-03-24 19:42:23 +0530

    Day56 Striver DSA Sheet

---
+++ Day56/SymmetricTree.java
@@ -0,0 +1,52 @@
+package Day56;
+
+public class SymmetricTree {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(3);
+
+        SymmetricTree solution = new SymmetricTree();
+
+        boolean result = solution.isSymmetric(root);
+        System.out.println("Is the tree symmetric? " + result);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return (root == null) || (isSymmetric(root.left, root.right));
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
